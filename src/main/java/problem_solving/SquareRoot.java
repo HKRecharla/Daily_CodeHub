ea16b609c355fa8aa43c999fe65cdd2aa693cc73
harsha4030hari <harikrishnarecharla@gmail.com>
2021-08-04 18:33:09 +0530

    updated sqrt

--- src/main/java/problem_solving/SquareRoot.java
+++ src/main/java/problem_solving/SquareRoot.java
@@ -3,10 +3,13 @@ package problem_solving;
 public class SquareRoot {
 
     public static void main(String[] args) {
-        int N=10;
+        int N=2147483647;
 
-        int v = sqrt(1);
+        int v = flor_sqrt(N);
         System.out.println(Math.floor(v));
+
+
+
     }
 
 
@@ -22,6 +25,30 @@ public class SquareRoot {
         return temp;
     }
 
+
+    public static int flor_sqrt(int A){
+
+        int start=1;
+        int end=A;
+        int ans=0;
+
+        if(A==0 || A==1) return A;
+
+        while (start<=end){
+            int mid = (int)Math.floor((start+end)/2);
+            if(mid*mid ==A) return mid;
+
+            if (mid*mid <A){
+                start = mid+1;
+                ans=mid;
+            }else{
+                end=mid-1;
+            }
+        }
+
+        return ans;
+
+    }
 
 
 }
